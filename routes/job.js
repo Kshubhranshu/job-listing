e7783da6d24fa23c5351c518db9956fa8ecec6bf
kumaar <kumaar@DESKTOP-UL0FUTT.localdomain>
2024-01-20 22:36:11 +0530

    added jobs apis

--- routes/job.js
+++ routes/job.js
@@ -29,4 +29,82 @@ router.post("/create", jwtVerify, async (req, res) => {
     }
 });
 
+router.post("/edit/:jobId", jwtVerify, async (req, res) => {
+    try {
+        const { companyName, logoUrl, title, description } = req.body;
+        const jobId = req.params.jobId;
+
+        if (!companyName || !logoUrl || !title || !description || !jobId) {
+            return res.status(400).json({
+                errorMessage: "Bad Request",
+            });
+        }
+
+        await Job.updateOne(
+            { _id: jobId },
+            {
+                $set: {
+                    companyName,
+                    logoUrl,
+                    title,
+                    description,
+                },
+            }
+        );
+
+        res.json({ message: "Job details updated successfully" });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/job-description/:jobId", async (req, res) => {
+    try {
+        const jobId = req.params.jobId;
+
+        if (!jobId) {
+            return res.status(400).json({
+                errorMessage: "Bad Request",
+            });
+        }
+
+        const jobDetails = await Job.findById(jobId);
+
+        res.json({ data: jobDetails });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/all", async (req, res) => {
+    try {
+        const title = req.query.title || "";
+        const jobList = await Job.find(
+            { title: { $regex: title, $options: "i" } },
+            { companyName: 1 }
+        );
+
+        // add filter in the find query with skills
+        // ["html", "css", "js"] this is how skill should be saved in database document
+
+        res.json({ data: jobList });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.delete("/job/:jobId", async (req, res) => {
+    try {
+        const title = req.query.title || "";
+        const jobList = await Job.deleteById(jobId);
+
+        // add filter in the find query with skills
+        // ["html", "css", "js"] this is how skill should be saved in database document
+
+        res.json({ data: jobList });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 module.exports = router;
